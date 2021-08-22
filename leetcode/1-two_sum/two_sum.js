750a32e14957a03292cb9bbab85a418f6b064208
plan-do-break-fix <jeffdswan@outlook.com>
2021-08-22 09:36:01 -0500

    solutions(leetcode): hashmap Two Sum solutions in JS and Python

--- leetcode/1-two_sum/two_sum.js
+++ leetcode/1-two_sum/two_sum.js
@@ -3,12 +3,35 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
+var nestedIterationTwoSum = function(nums, target) {
     for (let i=0; i < nums.length; i++) {
         for (let j=0; j < nums.length; j++) {
             if (i != j && nums[i] + nums[j] === target) {
                 return [i, j];
             }
         }
+    }
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// ECMAScript 2015 using Map()
+// Runtime: 688 ms
+// Memory Usage: 45.8 MB
+var hashMap2015TwoSum = function(nums, target) {
+    const table = new Map();
+    table.set(nums[0], 0);
+    for (let i=1; i < nums.length; i++) {
+        tableIter = table.entries();
+        for (let j=0; j < table.size; j++) { 
+            entry = tableIter.next().value;
+            if (target - entry[0] === nums[i] && entry[1] != i) {
+                return [entry[1], i];
+            }
+        }
+        table.set(nums[i], i); 
     }
 };
--- leetcode/1-two_sum/two_sum.py
+++ leetcode/1-two_sum/two_sum.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 from typing import List
 
-class Solution:
+class HashMapSolution:
+    """
+    Runtime: 2056 ms
+    Memory Usage: 14.2 MB
+    """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        table = {nums[0]: 0}
+        for i in range(1,len(nums)):
+            for num in table:
+                if target - nums[i] == num:
+                    return [table[num], i]
+            table[nums[i]] = i
+
+class DoubleIterationSolution:
+    """
+    Runtime: 8904 ms
+    Memory Usage: 14.9 MB
+    """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for _i, x in enumerate(nums):
             for _j, y in enumerate(nums):
